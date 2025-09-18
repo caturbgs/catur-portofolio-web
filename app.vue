d34c6cd8a8fb8b8b8a0a6f44ed1339df8d130072
Catur Bagaskara <caturbagas11@gmail.com>
2025-09-18 17:30:31 +0700

    Fix: Update favicon paths for deployment

--- app.vue
+++ app.vue
@@ -6,12 +6,12 @@ useHead({
     { name: "viewport", content: "width=device-width, initial-scale=1" },
   ],
   link: [
-    { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-    { rel: "icon", type: "image/png", href: "/favicon-16x16.png", sizes: "16x16" },
-    { rel: "icon", type: "image/png", href: "/favicon-32x32.png", sizes: "32x32" },
-    { rel: "icon", type: "image/png", href: "/android-chrome-192x192.png", sizes: "192x192" },
-    { rel: "icon", type: "image/png", href: "/android-chrome-512x512.png", sizes: "512x512" },
-    { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
+    { rel: "icon", type: "image/x-icon", href: "/catur-portofolio-web/favicon.ico" },
+    { rel: "icon", type: "image/png", href: "/catur-portofolio-web/favicon-16x16.png", sizes: "16x16" },
+    { rel: "icon", type: "image/png", href: "/catur-portofolio-web/favicon-32x32.png", sizes: "32x32" },
+    { rel: "icon", type: "image/png", href: "/catur-portofolio-web/android-chrome-192x192.png", sizes: "192x192" },
+    { rel: "icon", type: "image/png", href: "/catur-portofolio-web/android-chrome-512x512.png", sizes: "512x512" },
+    { rel: "apple-touch-icon", href: "/catur-portofolio-web/apple-touch-icon.png" },
   ],
   bodyAttrs: {
     class: "font-sans antialiased",
@@ -22,8 +22,7 @@ useHead({
 <template>
   <Header />
   <main
-    class="container px-3 sm:p-8 md:max-w-screen-md min-h-[calc(100vh-(var(--header-height-mobile)*2))] md:min-h-[calc(100vh-(var(--header-height)*2))]"
-  >
+    class="container px-3 sm:p-8 md:max-w-screen-md min-h-[calc(100vh-(var(--header-height-mobile)*2))] md:min-h-[calc(100vh-(var(--header-height)*2))]">
     <NuxtPage />
   </main>
   <Footer />
