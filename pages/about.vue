deb31bc5e65e693762f49755c4c4f2b6c0538d77
Catur Bagaskara <caturbagas11@gmail.com>
2025-09-19 11:45:28 +0700

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute()
+const { data: doc } = await useAsyncData(route.path, () => {
+  return queryCollection('content').path(route.path).first()
+})
+</script>
 
 <template>
   <h1 class="text-3xl font-bold">About</h1>
   <div class="space-y-4 mt-2 [&_a]:underline [&_a:hover]:text-primary">
-    <ContentDoc v-slot="{ doc }">
-      <ContentRenderer :value="doc" />
-    </ContentDoc>
+    <ContentRenderer v-if="doc" :value="doc" />
   </div>
 </template>
